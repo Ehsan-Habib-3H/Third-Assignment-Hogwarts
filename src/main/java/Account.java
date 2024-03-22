2e5bd62f33c874234695513cb22f1d065bf4a270
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2024-03-22 12:21:45 +0330

    hash added

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,16 +1,23 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.UUID;
 
 public class Account implements AccountManagement {
     private String username;
-    // TODO: Passwords should hashed
-    private String password;
+//    private String password;
+    private byte[] password;
     public UUID accountID;
 
     @Override
     public boolean validatePassword(String enteredPassword) {
-        //TODO
-        return (Objects.equals(password, enteredPassword));
+        try {
+            return authenticate(enteredPassword, password);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -21,10 +28,22 @@ public class Account implements AccountManagement {
 
     @Override
     public void changePassword(String newPassword) {
-        password = newPassword;
+        try {
+            password = hashPassword(newPassword);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getUsername() {
         return username;
+    }
+    private static byte[] hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        return md.digest(password.getBytes(StandardCharsets.UTF_8));
+    }
+    private static boolean authenticate(String input, byte[] hashedPassword) throws NoSuchAlgorithmException {
+        byte[] hashedInput = hashPassword(input);
+        return Arrays.equals(hashedInput, hashedPassword);
     }
 }
